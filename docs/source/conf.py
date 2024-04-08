72f490ca47d9322b1251cd80646398c824ab2e34
henry-e-n <nelliot@live.unc.edu>
2024-04-08 16:41:27 -0500

    added Sphinx documentation backend

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Cryogenic Materials Repository'
+copyright = '2024, Henry Nachman, Nicholas Galitzki, Oorie Desai'
+author = 'Henry Nachman, Nicholas Galitzki, Oorie Desai'
+release = '04-08-2024'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
