ddc340b47079129e2316be739bab11ce0da9fe18
henry-e-n <nelliot@live.unc.edu>
2024-04-08 17:23:06 -0500

    added sphinx extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,10 @@ release = '04-08-2024'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    ...
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
