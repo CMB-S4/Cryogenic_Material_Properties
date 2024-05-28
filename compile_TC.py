7550a8b408eed6cdbe3f6effe8cca602474757ad
henry-e-n <nelliot@live.unc.edu>
2024-05-28 09:52:15 -0500

    csv and txt changes

--- compile_TC.py
+++ compile_TC.py
@@ -16,14 +16,17 @@ mat_directories = [folder for folder in os.listdir(path_to_lib) if not folder.en
 path_to_fits = dict()
 path_to_nistfits = dict()
 path_to_otherfits = dict()
+path_to_rawData = dict()
 
 for mat in mat_directories:
     mat_str = f"{path_to_lib}\\{mat}"
     fit_str = f"{mat_str}\\fits"
     other_str = f"{mat_str}\\OTHERFITS"
     nist_str = f"{mat_str}\\NIST"
+    raw_str = f"{mat_str}\\RAW"
     if os.path.exists(fit_str):
         path_to_fits[mat] = fit_str
+        path_to_rawData[mat] = fit_str
     elif os.path.exists(other_str):
         path_to_fits[mat] = other_str
     elif os.path.exists(nist_str):
@@ -47,4 +50,8 @@ create_tc_csv(output_array, f"..\\thermal_conductivity_compilation_NIST_{current
 
 output_array = compile_csv(path_to_otherfits)
 create_data_table(output_array, f"..\\other_fits_{current_date}.txt")
-create_tc_csv(output_array, f"..\\other_fits_NIST_{current_date}.csv")
+create_tc_csv(output_array, f"..\\other_fits_{current_date}.csv")
+
+output_array = compile_csv(path_to_rawData)
+create_data_table(output_array, f"..\\rawData_Fits_{current_date}.txt")
+create_tc_csv(output_array, f"..\\rawData_Fits_{current_date}.csv")
