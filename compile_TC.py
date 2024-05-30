a2c31a5e4b68aab14d29cb16e10c85c2afaa8482
henry-e-n <nelliot@live.unc.edu>
2024-05-30 17:16:20 -0500

    added new fit types

--- compile_TC.py
+++ compile_TC.py
@@ -13,45 +13,78 @@ from thermal_conductivity.tc_utils import *
 path_to_lib = f"{os.getcwd()}\\lib"
 mat_directories = [folder for folder in os.listdir(path_to_lib) if not folder.endswith(".md")]
 
-path_to_fits = dict()
-path_to_nistfits = dict()
+everything_bagel = dict()
+simple_bagel = dict()
 path_to_otherfits = dict()
 path_to_rawData = dict()
 
-for mat in mat_directories:
-    mat_str = f"{path_to_lib}\\{mat}"
-    fit_str = f"{mat_str}\\fits"
-    other_str = f"{mat_str}\\OTHERFITS"
-    nist_str = f"{mat_str}\\NIST"
-    raw_str = f"{mat_str}\\RAW"
-    if os.path.exists(fit_str):
-        path_to_fits[mat] = fit_str
-        path_to_rawData[mat] = fit_str
-    elif os.path.exists(other_str):
-        path_to_fits[mat] = other_str
-    elif os.path.exists(nist_str):
-        path_to_fits[mat] = nist_str
+def make_pathtofit(mat_direct, subset=None, fits_to_parse="ALL"):
+    path_to_fit_dict = dict()
+    if subset!=None:
+        subset_array = []
+        for mat in mat_direct:
+            if mat in subset:
+                subset_array = np.append(subset_array, mat)
+        mat_direct = subset_array
+    for mat in mat_direct:
+        mat_str = f"{path_to_lib}\\{mat}"
+        fit_str = f"{mat_str}\\fits"
+        other_str = f"{mat_str}\\OTHERFITS"
+        nist_str = f"{mat_str}\\NIST"
+        raw_str = f"{mat_str}\\RAW"
+        
+        if fits_to_parse=="ALL":
+            if os.path.exists(fit_str): # Prioritize RAW fits
+                path_to_fit_dict[mat] = fit_str
+                path_to_rawData[mat] = fit_str
+            elif os.path.exists(other_str): # Then other fits
+                path_to_fit_dict[mat] = other_str
+            elif os.path.exists(nist_str): # Lastly NIST Fits
+                path_to_fit_dict[mat] = nist_str
 
-    if os.path.exists(nist_str):
-        path_to_nistfits[mat] = nist_str
-    if os.path.exists(other_str):
-        path_to_otherfits[mat] = other_str
+        if fits_to_parse=="OTHER":
+            if os.path.exists(other_str): # Then other fits
+                path_to_fit_dict[mat] = other_str
+            elif os.path.exists(nist_str): # Lastly NIST Fits
+                path_to_fit_dict[mat] = nist_str
 
-output_array = compile_csv(path_to_fits)
+        if fits_to_parse=="RAW":
+            if os.path.exists(raw_str): # Prioritize RAW fits
+                path_to_fit_dict[mat] = fit_str
+    
+    return path_to_fit_dict
 
 current_date = datetime.now().date()
 
-create_data_table(output_array, f"..\\thermal_conductivity_compilation_{current_date}.txt")
-create_tc_csv(output_array, f"..\\thermal_conductivity_compilation_{current_date}.csv")
+# Want to create 4 output files
+# 1. Plain Bagel : Simple file that has only 1 fit per material and ignores weird materials
+simple_mat_direct = ["Aluminum_110", "Beryllium_Copper","Brass","CFRP","Constantan","Cu_OFHC_RRR50",
+                     "G10_FR4","Glass_FabricPolyester_He_warp","Graphite","Inconel_718","Invar_Fe36Ni",
+                     "Iron","Kapton","Ketron","Kevlar49_Composite_Aramid","Lead","Macor","Manganin",
+                     "Molybdenum","MylarPET","NbTi","Nichrome","Nickel_Steel_Fe_2.25_Ni","Nylon",
+                     "Phosbronze","Platinum","Polystyrene_2.0_lbft3","Polyurethane_2.0_lbft3_CO2",
+                     "PVC_1.25_lbft3_air","Stainless_Steel","Teflon","Ti6Al4V","Titanium_15333",
+                     "Torlon","Tungsten","VESPEL"]
+simple_bagel = make_pathtofit(mat_directories, subset=simple_mat_direct)
+output_array = compile_csv(simple_bagel)
+create_data_table(output_array, f"..\\tc_generic_{current_date}.txt")
+create_tc_csv(output_array, f"..\\tc_generic_{current_date}.csv")
 
-output_array = compile_csv(path_to_nistfits)
-create_data_table(output_array, f"..\\thermal_conductivity_compilation_NIST_{current_date}.txt")
-create_tc_csv(output_array, f"..\\thermal_conductivity_compilation_NIST_{current_date}.csv")
 
-output_array = compile_csv(path_to_otherfits)
-create_data_table(output_array, f"..\\other_fits_{current_date}.txt")
-create_tc_csv(output_array, f"..\\other_fits_{current_date}.csv")
+# 2. Everything Bagel : File that contains every single material and alloy
+everything_bagel = make_pathtofit(mat_directories, fits_to_parse="ALL")
+output_array = compile_csv(everything_bagel)
+create_data_table(output_array, f"..\\tc_fullrepo_{current_date}.txt")
+create_tc_csv(output_array, f"..\\tc_fullrepo_{current_date}.csv")
 
-output_array = compile_csv(path_to_rawData)
-create_data_table(output_array, f"..\\rawData_Fits_{current_date}.txt")
-create_tc_csv(output_array, f"..\\rawData_Fits_{current_date}.csv")
+# 3. Other fits + NIST
+other_fits = make_pathtofit(mat_directories, fits_to_parse="OTHER")
+output_array = compile_csv(other_fits)
+create_data_table(output_array, f"..\\tc_other_fits_{current_date}.txt")
+create_tc_csv(output_array, f"..\\tc_other_fits_{current_date}.csv")
+
+# 4. RAW / from data fits
+raw_fits = make_pathtofit(mat_directories, fits_to_parse="RAW")
+output_array = compile_csv(raw_fits)
+create_data_table(output_array, f"..\\tc_rawdata_fits_{current_date}.txt")
+create_tc_csv(output_array, f"..\\tc_rawdata_fits_{current_date}.csv")
--- thermal_conductivity/fit_types.py
+++ thermal_conductivity/fit_types.py
@@ -47,20 +47,20 @@ def loglog_func(T, low_param, hi_param, erf_param, erf_multiplicity=15):
 def power_law(T, params):
     A, B = params
 
-    k = (A*(10**(-3)))*T**(B)
+    k = (A)*T**(B)
 
     return k
 
 #################################################################
+# From the NIST website
 
 def NIST1(T, params):
     k = 10**np.polyval(params, np.log10(T))
     return k
 
 
-
 #################################################################
-
+# From Ray Radebaugh
 def RRadebaugh1(T, low_param, hi_param, erf_param):
     low_param = np.flip(low_param)
     hi_param = np.flip(hi_param)
@@ -120,3 +120,269 @@ def RRadebaugh3(T, low_param, hi_param, erf_param):
 
     k = low_fit*erf_low+hi_fit*erf_hi
     return k
+
+###########################################################################
+# From the NIST5a Excel Spreadsheet
+
+def NIST5a_1(T, params):
+    k = 10**np.polyval(params[::-1], np.log10(T))
+    return k
+
+"""
+Case 1   'NIST type formulation, see first sheet
+    logtemp = Log(temp) / Log(10#)
+    For I = 1 To 15
+        Conductivity = Conductivity + IParameters(I) * logtemp ^ (I - 1)
+    Next I
+    Conductivity = 10 ^ Conductivity
+"""
+
+def NIST5a_2(T, params):
+    k = params[0] + params[2] * T ** (0.5) + params[4] * T + params[6] * T ** (1.5) + params[8] * T ** (2)
+    k = k / (1 + params[1] * T ** (0.5) + params[3] * T + params[5] * T ** (1.5) + params[7] * T ** (2))
+    k = 10 ** k
+    return k
+"""
+Case 2  'NIST formulation for OFHC Copper
+    Conductivity = IParameters(1) + IParameters(3) * temp ^ (0.5) + IParameters(5) * temp + IParameters(7) * temp ^ (1.5) + IParameters(9) * temp ^ (2#)
+    Conductivity = Conductivity / (1 + IParameters(2) * temp ^ (0.5) + IParameters(4) * temp + IParameters(6) * temp ^ (1.5) + IParameters(8) * temp ^ (2#))
+    Conductivity = 10 ^ Conductivity
+
+"""
+
+def NIST5a_3(T, params):
+    lnT = np.log(T)
+    x = ((lnT - params[1]) - (params[2] - lnT)) / (params[2] - params[1])
+    k = 0
+    for i in range(3,3+params[0]):
+        k = k + params(i) * np.cos((i - 3) * np.arccos(x))
+    k = np.exp(k)
+    return k
+"""
+Case 3  'Tcheby polynomial in ln(T) giving ln(g), a=# parameters, b=low temp, c= high temp, d...=parameters
+    x = Log(temp) / Log(2.71828)
+    x = ((x - IParameters(2)) - (IParameters(3) - x)) / (IParameters(3) - IParameters(2))
+    For I = 4 To 3 + IParameters(1)
+        Conductivity = Conductivity + IParameters(I) * Cos((I - 4) * Application.WorksheetFunction.Acos(x))
+    Next I
+    Conductivity = 2.71828 ^ Conductivity
+    
+"""
+def NIST5a_4(T, params):
+    k = 0
+    if T > params[10]:
+        logtemp = np.log10(T)
+        for i in range(1, 10):
+            k += params[i - 1] * logtemp ** (i - 1)
+        k = 10 ** k
+    elif T > params[11]:
+        k = params[13] * T ** params[12]
+    else:
+        k = params[16] * T ** params[15]
+    
+    return k
+
+"""
+Case 4 'Extrapolation to lower temp, either by using a*T^1.8, or better with data
+    'column j:temp to start transition
+    'column k:temp to end transition
+    'column l:power
+    'column m:coefficient
+    'column n:estimate of lower validity
+    'column o:power
+    'column p:coefficient
+       
+    
+    If (temp > IParameters(10)) Then
+        logtemp = Log(temp) / Log(10#)
+        For I = 1 To 9
+            Conductivity = Conductivity + IParameters(I) * logtemp ^ (I - 1)
+        Next I
+        Conductivity = 10 ^ Conductivity
+    ElseIf (temp > IParameters(11)) Then
+        Conductivity = IParameters(13) * temp ^ IParameters(12)
+    Else
+        Conductivity = IParameters(16) * temp ^ IParameters(15)
+    End If
+    
+"""
+
+
+def NIST5a_5(temp, IParameters):
+    L_0 = 0.00000002443
+    beta = IParameters[1] / L_0 * 1 / IParameters[0]
+    
+    ln_temp_ratio_10 = np.log(temp / IParameters[10])
+    ln_temp_ratio_11 = np.log(temp / IParameters[11])
+    ln_temp_ratio_14 = np.log(temp / IParameters[14])
+    ln_temp_ratio_15 = np.log(temp / IParameters[15])
+    ln_temp_ratio_18 = np.log(temp / IParameters[18])
+    ln_temp_ratio_19 = np.log(temp / IParameters[19])
+    ln_temp_ratio_22 = np.log(temp / IParameters[22])
+
+    rho_c = (IParameters[9] * ln_temp_ratio_10 * np.exp(-1 * (ln_temp_ratio_11 / IParameters[12]) ** 2) +
+             IParameters[13] * ln_temp_ratio_14 * np.exp(-1 * (ln_temp_ratio_15 / IParameters[16]) ** 2) +
+             IParameters[17] * ln_temp_ratio_18 * np.exp(-1 * (ln_temp_ratio_19 / IParameters[20]) ** 2) +
+             IParameters[21] * np.exp(-1 * (ln_temp_ratio_22 / IParameters[23]) ** 2))
+
+    rho_0 = beta / temp
+
+    rho_i = (IParameters[2] * temp ** IParameters[3] / 
+             (1 + IParameters[2] * IParameters[4] * temp ** (IParameters[3] + IParameters[5]) * 
+              np.exp(-1 * (IParameters[6] / temp) ** IParameters[7])) + rho_c)
+    
+    rho_i0 = IParameters[8] * rho_i * rho_0 / (rho_i + rho_0)
+    
+    Conductivity = 1 / (rho_0 + rho_i + rho_i0)
+    
+    return Conductivity
+
+"""
+Case 5 'Classic NBS equations for Al, Cu, Fe, W using measured RRR values
+        'See NBSIR 84-3007 June 1984 Thermal Conductivity of Al, Cu, Fe, W
+        'for temperatures from 1K to the melting point
+    L_0 = 0.00000002443
+    beta = IParameters(1) / L_0 * 1 / IParameters(0)
+    rho_c = IParameters(9) * Ln(temp / IParameters(10)) * Exp(-1 * (Ln(temp / IParameters(11)) / IParameters(12)) ^ 2) + _
+           IParameters(13) * Ln(temp / IParameters(14)) * Exp(-1 * (Ln(temp / IParameters(15)) / IParameters(16)) ^ 2) + _
+           IParameters(17) * Ln(temp / IParameters(18)) * Exp(-1 * (Ln(temp / IParameters(19)) / IParameters(20)) ^ 2) + _
+           IParameters(21) * Exp(-1 * (Ln(temp / IParameters(22)) / IParameters(23)) ^ 2)
+    rho_0 = beta / temp
+    rho_i = IParameters(2) * temp ^ IParameters(3) / (1 + IParameters(2) * IParameters(4) * temp ^ (IParameters(3) + IParameters(5)) * _
+            Exp(-1 * (IParameters(6) / temp) ^ IParameters(7))) + rho_c
+    rho_i0 = IParameters(8) * rho_i * rho_0 / (rho_i + rho_0)
+    
+    Conductivity = 1 / (rho_0 + rho_i + rho_i0)
+    
+
+"""
+
+def NIST5a_6(temp, IParameters, sc):
+    if sc == 1:
+        temp = 1.18
+
+    L_0 = 0.0000000245
+    beta = IParameters[1] / L_0 / IParameters[0]
+
+    IParameters[2] = min(IParameters[24] * (IParameters[0] ** IParameters[25]), IParameters[2])
+    IParameters[4] = max(IParameters[26] * (IParameters[0] ** IParameters[27]), IParameters[4])
+
+    ln_temp_ratio_10 = np.log(temp / IParameters[10])
+    ln_temp_ratio_11 = np.log(temp / IParameters[11])
+    ln_temp_ratio_14 = np.log(temp / IParameters[14])
+    ln_temp_ratio_15 = np.log(temp / IParameters[15])
+    ln_temp_ratio_18 = np.log(temp / IParameters[18])
+    ln_temp_ratio_19 = np.log(temp / IParameters[19])
+    ln_temp_ratio_22 = np.log(temp / IParameters[22])
+
+    rho_c = (IParameters[9] * ln_temp_ratio_10 * np.exp(-1 * (ln_temp_ratio_11 / IParameters[12]) ** 2) +
+             IParameters[13] * ln_temp_ratio_14 * np.exp(-1 * (ln_temp_ratio_15 / IParameters[16]) ** 2) +
+             IParameters[17] * ln_temp_ratio_18 * np.exp(-1 * (ln_temp_ratio_19 / IParameters[20]) ** 2) +
+             IParameters[21] * np.exp(-1 * (ln_temp_ratio_22 / IParameters[23]) ** 2))
+
+    rho_0 = beta / temp
+
+    rho_i = (IParameters[2] * temp ** IParameters[3] / 
+             (1 + IParameters[2] * IParameters[4] * temp ** (IParameters[3] + IParameters[5]) * 
+              np.exp(-1 * (IParameters[6] / temp) ** IParameters[7])) + rho_c)
+
+    rho_i0 = IParameters[8] * rho_i * rho_0 / (rho_i + rho_0)
+
+    if sc == 1:
+        normcond = 1 / (rho_0 + rho_i + rho_i0)
+        formtype = 8
+        return normcond, formtype  # Simulates the "GoTo Reenter"
+    else:
+        Conductivity = 1 / (rho_0 + rho_i + rho_i0)
+        return Conductivity
+"""
+Case 6 'Modified NBS approach by Adam Woodcraft: Cryogenics 45 (2005) 421–431
+    
+    If (sc = 1) Then temp = 1.18
+    
+    L_0 = 0.0000000245
+    beta = IParameters(1) / L_0 * 1 / IParameters(0)
+    
+    IParameters(2) = Application.WorksheetFunction.Min(IParameters(24) * (IParameters(0)) ^ IParameters(25), IParameters(2))
+    IParameters(4) = Application.WorksheetFunction.Max(IParameters(26) * (IParameters(0)) ^ IParameters(27), IParameters(4))
+    
+    rho_c = IParameters(9) * Ln(temp / IParameters(10)) * Exp(-1 * (Ln(temp / IParameters(11)) / IParameters(12)) ^ 2) + _
+           IParameters(13) * Ln(temp / IParameters(14)) * Exp(-1 * (Ln(temp / IParameters(15)) / IParameters(16)) ^ 2) + _
+           IParameters(17) * Ln(temp / IParameters(18)) * Exp(-1 * (Ln(temp / IParameters(19)) / IParameters(20)) ^ 2) + _
+           IParameters(21) * Exp(-1 * (Ln(temp / IParameters(22)) / IParameters(23)) ^ 2)
+    rho_0 = beta / temp
+    rho_i = IParameters(2) * temp ^ IParameters(3) / (1 + IParameters(2) * IParameters(4) * temp ^ (IParameters(3) + IParameters(5)) * _
+            Exp(-1 * (IParameters(6) / temp) ^ IParameters(7))) + rho_c
+    rho_i0 = IParameters(8) * rho_i * rho_0 / (rho_i + rho_0)
+    
+    If (sc = 1) Then
+        normcond = 1 / (rho_0 + rho_i + rho_i0)
+        formtype = 8
+        GoTo Reenter
+    Else
+        Conductivity = 1 / (rho_0 + rho_i + rho_i0)
+    End If
+
+"""
+def NIST5a_7(temp, IParameters):
+    if temp > IParameters[17]:
+        formtype = IParameters[16]
+        # Simulate the "GoTo Reenter" by returning formtype and a flag
+        return None, formtype
+    else:
+        Conductivity = IParameters[19] * temp ** IParameters[18]
+        return Conductivity, None
+
+
+"""
+Case 7 'First do higher temp, the continue with power-series approximation.
+    If (temp > IParameters(17)) Then
+        formtype = IParameters(16)
+        GoTo Reenter
+    Else
+        Conductivity = IParameters(19) * temp ^ IParameters(18)
+    End If
+
+    
+
+"""
+
+def NIST5a_7(temp, IParameters, sc, normcond=None):
+    if temp > IParameters[30]:
+        formtype = IParameters[29]
+        # Simulate the "GoTo Reenter" by returning formtype and a flag
+        return None, formtype, sc, None
+    elif IParameters[29] == 6:
+        if sc == 0:
+            sc = 1
+            origtemp = temp
+            formtype = 6
+            # Simulate the "GoTo Reenter" by returning formtype and a flag
+            return None, formtype, sc, origtemp
+        else:
+            temp = origtemp
+            Conductivity = normcond * np.exp(IParameters[31] * (1 - 1.18 / temp))
+            return Conductivity, None, sc, None
+    else:
+        Conductivity = IParameters[32] * np.exp(IParameters[31] * (1 - IParameters[30] / temp))
+        return Conductivity, None, sc, None
+
+"""
+Case 8 'Superconducting extension
+    If (temp > IParameters(30)) Then
+        formtype = IParameters(29)
+        GoTo Reenter
+    ElseIf (IParameters(29) = 6) Then
+        If (sc = 0) Then
+            sc = 1
+            origtemp = temp
+            formtype = 6
+            GoTo Reenter
+        Else
+            temp = origtemp
+            Conductivity = normcond * Exp(IParameters(31) * (1 - 1.18 / temp))
+        End If
+    Else
+        Conductivity = IParameters(32) * Exp(IParameters(31) * (1 - IParameters(30) / temp))
+    End If  
+"""
